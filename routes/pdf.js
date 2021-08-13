dfb192395e3b7a65594516774f95d2075855c5f2
Elias Peeters <Elias.peeters@otto.de>
2021-08-13 11:28:41 +0200

    Added new task

--- routes/pdf.js
+++ routes/pdf.js
@@ -6,7 +6,13 @@ const uuid = require('uuidv4');
 var options = {
     format: "A4",
     orientation: "portrait",
-    border: "10mm"
+    border: "10mm",
+    header: {
+        height: "1mm",
+    },
+    footer: {
+        height: "1mm",
+    }
 };
 
 var htmlTask = fs.readFileSync("tasks.html", "utf8");
@@ -16,7 +22,9 @@ app.get('/createpdf', (req, res) => {
     let name = uuid.uuid()
     let binaryToDezimalLength = (req.query.binaryToDezimal != null) ? req.query.binaryToDezimal: 10;
     let dezimalToBinaryLength = (req.query.dezimalToBinary != null) ? req.query.dezimalToBinary: 10;
-
+    let binaryAddtionLength = (req.query.binaryAddition != null) ? req.query.binaryAddition: 10;
+    console.log(binaryAddtionLength)
+    console.log(req.query)
 
     let binaryToDezimal = []
     for (let i = 0; i < binaryToDezimalLength; i++) {
@@ -27,7 +35,7 @@ app.get('/createpdf', (req, res) => {
         })
     }
 
-    dezimalToBinary = []
+    let dezimalToBinary = []
     for (let i = 0; i < dezimalToBinaryLength; i++) {
         let task = Math.round(Math.random() * 255);
         dezimalToBinary.push({
@@ -36,11 +44,25 @@ app.get('/createpdf', (req, res) => {
         })
     }
 
+    let binaryAddtion = []
+    for (let i = 0; i < binaryAddtionLength; i++) {
+        let task = {
+            number1: context.randomDualNumber(2, 10),
+            number2: context.randomDualNumber(2, 10),
+        };
+        binaryAddtion.push({
+            task: task,
+            solution: context.additionDual(task.number1, task.number2)
+        })
+    }
+
+
     var documentTask = {
         html: htmlTask,
         data: {
             binaryToDezimal: binaryToDezimal,
-            dezimalToBinary: dezimalToBinary
+            dezimalToBinary: dezimalToBinary,
+            binaryAddtion: binaryAddtion
         },
         path: `./pdfs/${name}Task.pdf`,
         type: "",
@@ -50,7 +72,8 @@ app.get('/createpdf', (req, res) => {
         html: htmlSolution,
         data: {
             binaryToDezimal: binaryToDezimal,
-            dezimalToBinary: dezimalToBinary
+            dezimalToBinary: dezimalToBinary,
+            binaryAddtion: binaryAddtion
         },
         path: `./pdfs/${name}Solution.pdf`,
         type: "",
